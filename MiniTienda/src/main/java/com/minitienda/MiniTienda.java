b9a30fd2d513a792302e2daf8e73f52ca4034322
Jogo <joseesapel@gmail.com>
2025-10-21 18:21:07 -0500

    cree l ui de la semana  1

--- MiniTienda/src/main/java/com/minitienda/MiniTienda.java
+++ MiniTienda/src/main/java/com/minitienda/MiniTienda.java
@@ -1,9 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package com.minitienda;
-
 /**
  *
  * @author Coder
@@ -11,6 +6,5 @@ package com.minitienda;
 public class MiniTienda {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
     }
 }
